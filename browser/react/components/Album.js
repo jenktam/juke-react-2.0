30fd6660cacc2cc27079636c7cac6efda41e4fed
aperez25 <alice.perez25@gmail.com>
2017-05-16 15:00:45 -0700

    album and albums views working

--- browser/react/components/Album.js
+++ browser/react/components/Album.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import Songs from '../components/Songs';
+import { Link } from 'react-router';
 
-const Album = (props) => {
+class Album extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount () {
+    const albumId = this.props.routeParams.albumId;
+    const selectAlbum = this.props.selectAlbum;
 
-  const album = props.album;
-  const currentSong = props.currentSong;
-  const isPlaying = props.isPlaying;
-  const toggleOne = props.toggleOne;
+    selectAlbum(albumId);
+  }
 
-  return (
-    <div className="album">
-      <div>
-        <h3>{ album.name }</h3>
-        <img src={ album.imageUrl } className="img-thumbnail" />
+  render () {
+    const album = this.props.album;
+    const currentSong = this.props.currentSong;
+    const isPlaying = this.props.isPlaying;
+    const toggleOne = this.props.toggleOne;
+
+    return (
+      <div className="album">
+        <div>
+          <h3>{ album.name }</h3>
+            <img src={ album.imageUrl } className="img-thumbnail" />
+        </div>
+        <Songs
+          songs={album.songs}
+          currentSong={currentSong}
+          isPlaying={isPlaying}
+          toggleOne={toggleOne} />
       </div>
-      <Songs
-        songs={album.songs}
-        currentSong={currentSong}
-        isPlaying={isPlaying}
-        toggleOne={toggleOne} />
-    </div>
-  );
+    );
+  }
 }
 
 export default Album;
--- browser/react/components/Albums.js
+++ browser/react/components/Albums.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Albums = (props) => {
 
   const albums = props.albums;
-  const selectAlbum = props.selectAlbum;
 
   return (
     <div>
-      <h3>Albums</h3>
+      <Link to="/albums">
+        <h3>Albums</h3>
+      </Link>
       <div className="row">
       {
         albums.map(album => (
           <div className="col-xs-4" key={ album.id }>
-            <a className="thumbnail" href="#" onClick={() => selectAlbum(album.id)}>
-              <img src={ album.imageUrl } />
+            <a className="thumbnail" href="#">
+              <Link to={`/albums/${album.id}`}>
+                <img src={ album.imageUrl } />
+              </Link>
               <div className="caption">
                 <h5>
                   <span>{ album.name }</span>
